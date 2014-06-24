50af5b6accd8d3244a9a6cd279afbf8e7c91a154
Oren Miller <oren@ubuntu-linux.(none)>
2014-06-24 00:03:46 -0500

    Only clone to 1 level.  Delete .git directory before packaging

--- package.sh
+++ package.sh
@@ -2,7 +2,8 @@ QF_VERSION=$1
 
 rm -rf quickfix
 
-git clone https://github.com/quickfix/quickfix.git
+git clone --depth 1 https://github.com/quickfix/quickfix.git
+rm -rf quickfix/.git
 
 pushd quickfix/doc
 ./document.sh
