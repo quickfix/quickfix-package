a3d299cdebea1a8796ccf071d4b8e4b672a5bc2f
Oren Miller <oren@quickfixengine.org>
2014-06-24 00:02:15 -0700

    Delete .git directory before packaging

--- package.bat
+++ package.bat
@@ -7,6 +7,7 @@ set QF_VERSION=%2
 rmdir /s/q quickfix
 
 git clone --depth 1 https://github.com/quickfix/quickfix.git
+rmdir /s/q quickfix\.git
 
 pushd quickfix\doc
 call document.bat
@@ -17,6 +18,11 @@ del /Q quickfix.zip
 
 pushd quickfix
 call build_vs%VS_VERSION%.bat
+popd
+
+pushd quickfix\test
+call runut.bat release 11111
+call runat.bat release 11111
 popd
 
 7z a -tzip -xr!?git\* quickfix-bin-vs%VS_VERSION%-%QF_VERSION%.zip quickfix/include quickfix/lib/quickfix.lib quickfix/spec/FIX*.xml quickfix/doc/html 
