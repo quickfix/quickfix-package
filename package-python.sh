c25cafd187fde90b82cc51225d57d5f1a8912e69
Oren Miller <omiller@wud-omiller01.us.drwholdings.com>
2014-09-10 15:51:16 -0500

    Added package generator for pypi

---
+++ package-python.sh
@@ -0,0 +1,23 @@
+rm -rf quickfix-python/C++
+rm -rf quickfix-python/quickfix*.py
+rm -rf quickfix-python/doc
+rm -rf quickfix-python/LICENSE
+
+mkdir quickfix-python/C++
+
+cp quickfix/LICENSE quickfix-python
+
+cp quickfix/src/python/*.py quickfix-python
+cp quickfix/src/C++/*.h quickfix-python/C++
+cp quickfix/src/C++/*.hpp quickfix-python/C++
+cp quickfix/src/C++/*.cpp quickfix-python/C++
+cp quickfix/src/python/QuickfixPython.cpp quickfix-python/C++
+cp quickfix/src/python/QuickfixPython.h quickfix-python/C++
+
+touch quickfix-python/C++/config.h
+touch quickfix-python/C++/config_windows.h
+rm -f quickfix-python/C++/stdafx.*
+
+pushd quickfix-python
+
+python setup.py sdist upload -r test
---
+++ quickfix-python/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from distutils.core import Extension
+from distutils.command.install import install
+from distutils.command.build import build
+
+import subprocess
+import shutil
+import glob
+import os
+
+long_description=''
+with open('LICENSE') as file:
+    license = file.read();
+
+setup(name='quickfix',
+      version='1.14.0',
+      py_modules=['quickfix', 'quickfixt11', 'quickfix40', 'quickfix41', 'quickfix42', 'quickfix43', 'quickfix44', 'quickfix50', 'quickfix50sp1', 'quickfix50sp2'],
+      author='Oren Miller',
+      author_email='oren@quickfixengine.org',
+      maintainer='Oren Miller',
+      maintainer_email='oren@quickfixengine.org',
+      description="FIX (Financial Information eXchange) protocol implementation",
+      url='http://www.quickfixengine.org',
+      download_url='http://www.quickfixengine.org',
+      license=license,
+      include_dirs=['C++'],
+      ext_modules=[Extension('_quickfix', glob.glob('C++/*.cpp'))],
+)
