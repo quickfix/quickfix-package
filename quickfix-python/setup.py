ccd0f405584671b24e58a9ae2ce189fa687a2829
Oren Miller <oren@quickfixengine.org>
2018-04-15 22:26:17 -0500

    Updated version in python package.

--- quickfix-python/setup.py
+++ quickfix-python/setup.py
@@ -52,7 +52,7 @@ with open('LICENSE') as file:
     license = file.read();
 
 setup(name='quickfix',
-      version='0.0.1',
+      version='1.15.1',
       py_modules=['quickfix', 'quickfixt11', 'quickfix40', 'quickfix41', 'quickfix42', 'quickfix43', 'quickfix44', 'quickfix50', 'quickfix50sp1', 'quickfix50sp2'],
       data_files=[('share/quickfix', glob.glob('spec/FIX*.xml'))],
       author='Oren Miller',
