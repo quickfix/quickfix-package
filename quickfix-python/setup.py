8b9b1448201e932b690213a17d272dd13b6ee2a2
Nurettin Onur TUĞCU <onur.tugcu@gmail.com>
2021-12-24 16:45:03 +0300

    correct include_dirs in setup.py
    fixes https://github.com/quickfix/quickfix/issues/357

--- quickfix-python/setup.py
+++ quickfix-python/setup.py
@@ -63,7 +63,6 @@ setup(name='quickfix',
       url='http://www.quickfixengine.org',
       download_url='http://www.quickfixengine.org',
       license=license,
-      include_dirs=['C++'],
       cmdclass = {'build_ext': build_ext_subclass },
-      ext_modules=[Extension('_quickfix', glob.glob('C++/*.cpp'), extra_compile_args=['-std=c++0x', '-Wno-deprecated', '-Wno-unused-variable', '-Wno-deprecated-declarations', '-Wno-maybe-uninitialized'])],
+      ext_modules=[Extension('_quickfix', glob.glob('C++/*.cpp'), include_dirs=["C++"], extra_compile_args=['-std=c++0x', '-Wno-deprecated', '-Wno-unused-variable', '-Wno-deprecated-declarations', '-Wno-maybe-uninitialized'])],
 )
