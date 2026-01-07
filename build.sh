a13f2bfdf1e3f722e4aa16c8e9e99e68c7bfb623
Pablo Carballo <pablodcar@gmail.com>
2026-01-07 23:11:29 +0100

    test-wheels-py312

--- build.sh
+++ build.sh
@@ -3,23 +3,6 @@ WITH_MYSQL=${WITH_MYSQL:-0}
 
 arguments=(--with-python --with-openssl)
 
-# TODO: support building with mysql and postgresql support
-# if [ "$WITH_MYSQL" -eq 1 ]; then
-#     arguments+=(--with-mysql)
-# fi
-
-if [ -d quickfix ]; then
-  echo "quickfix directory already exists."
-  exit
-fi
-
-git clone https://github.com/pablodcar/quickfix
-rm -rf quickfix/.git
-
-# pushd quickfix/doc
-# ./document.sh
-# popd
-
 pushd quickfix || exit
 ./bootstrap
 ./configure "${arguments[@]}" && make
@@ -29,4 +12,11 @@ popd || exit
 pushd quickfix/src/python || exit
 echo "building Python interface..."
 ./swig.sh
+popd || exit
+
+./package-python.sh
+
+pushd quickfix-py || exit
+echo "building Python package..."
+python -m build
 popd || exit
